188433713746298b5ee9c9c8760c3125992a3ca3
mak4026 <chikuwa2135@gmail.com>
2017-09-20 17:36:44 +0900

    twitter_id validation

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,5 +6,9 @@ class TopController < ApplicationController
 
   def confirm
     @target_id = params[:twitter_id]
+    valid_id_regexp = Regexp.compile("[0-9a-zA-Z_]{1,15}")
+    if !valid_id_regexp.match?(@target_id)
+      redirect_to :root, alert: 'Twitterのスクリーンネーム(@から始まるID)を指定してください'
+    end
   end
 end
