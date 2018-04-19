a1aaa45a7566b59c6b9e47dc060d92302dee6286
mak4026 <chikuwa2135@gmail.com>
2018-04-19 13:55:58 +0900

    bug fix: ブロック/ミュート数が多い場合に，blocked_ids, muted_idsを取得しきれない問題を修正(多分)

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -20,8 +20,8 @@ class TopController < ApplicationController
       @target = client.user("@#{target_id}")
       @adq = advanced_query(params[:any_words], params[:none_words])
       search = client.search("to:#{target_id} #{@adq}", count: 100)
-      @blocked_ids = Set.new(client.blocked_ids.to_h[:ids])
-      @muted_ids = Set.new(client.muted_ids.to_h[:ids])
+      @blocked_ids = Set.new(client.blocked_ids.to_a)
+      @muted_ids = Set.new(client.muted_ids.to_a)
 
       @tweets = []
       search.each_slice(100){ |arr|
